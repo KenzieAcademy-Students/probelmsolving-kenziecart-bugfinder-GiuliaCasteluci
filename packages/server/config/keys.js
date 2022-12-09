79c94a7a5fb0550c633c8ecc9fa0e24597a6d0a9
GiuliaCasteluci <giulia.casteluci@mykenzie.snhu.edu>
2022-12-09 15:51:07 -0300

    done

--- packages/server/config/keys.js
+++ packages/server/config/keys.js
@@ -1,11 +1,15 @@
-export const app = {
-  name: 'Mern Social Media',
-  apiEndpoint: process.env.API_URL ? `${process.env.API_URL}` : 'api',
-};
-export const database = {
-  url: process.env.MONGODB_URI || 'mongodb+srv://giulia:Uz0QKJpGixupEPrx@cluster1.qjbu96s.mongodb.net/?retryWrites=true&w=majority',
-};
-export const jwt = {
-  secret: process.env.JWT_SECRET || 'jwt-secret',
-  tokenLife: '7d',
-};
+module.exports = {
+  app: {
+    name: 'Mern Social Media',
+    apiEndpoint: process.env.API_URL ? `${process.env.API_URL}` : 'api',
+  },
+  database: {
+    url: process.env.MONGODB_URI || 'mongodb+srv://giulia:Uz0QKJpGixupEPrx@cluster1.qjbu96s.mongodb.net/?retryWrites=true&w=majority',
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET || 'jwt-secret',
+    tokenLife: '7d',
+  },
+}
+
+//password Uz0QKJpGixupEPrx
